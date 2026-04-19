6c81301ffe30212c0999c6e2a5b991d877b4c6ed
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2026-04-19 02:38:49 +0200

    Update index.js

--- plugins/seo-pro/index.js
+++ plugins/seo-pro/index.js
@@ -1,3 +1,21 @@
 (function() {
-  // todo
-}();
+  console.log('[FME] Injecting tab link...');
+  const h = document.querySelector('#page-body > #tabs');
+  if (!h) {
+    console.warn('[FME] Nu am găsit containerul de taburi, nu pot injecta link-ul.');
+    return;
+  };
+
+  const link = document.createElement('li');
+  link.innerHTML = `<a href="#" id="fme-tab-link">FME Plugins</a>`;
+  h.querySelector('ul #admin_search').insertAdjacentElement('beforebegin', link);
+
+  MutationObserver.observe(h, { childList: true, subtree: true });
+
+  h.addEventListener('click', async (e) => {
+    if (e.target.matches('#fme-tab-link')) {
+      e.preventDefault();
+      console.log('[FME] Tab clicked, loading plugin manager...');
+    }
+  });
+})();
